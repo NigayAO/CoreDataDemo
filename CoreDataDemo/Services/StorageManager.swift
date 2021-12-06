62036857b5132a6d5c84b9f8e9f3a4c50d13aa9a
Alik <Nigay_AO@bk.ru>
2021-12-07 00:45:38 +0300

    Manage with file structure

---
+++ CoreDataDemo/Services/StorageManager.swift
@@ -0,0 +1,17 @@
+//
+//  StorageManager.swift
+//  CoreDataDemo
+//
+//  Created by Alik Nigay on 07.12.2021.
+//
+
+import Foundation
+import CoreData
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    
+}
